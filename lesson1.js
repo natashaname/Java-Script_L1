14716bc369980ecc0a2cd69120e7b6c9f7eb8762
natashaname <69076612+natashaname@users.noreply.github.com>
2021-10-10 23:37:34 +0300

    Add files via upload

---
+++ lesson1.js
@@ -0,0 +1,41 @@
+let firstNumb, secondNumb
+firstNumb = 29
+secondNumb = 12
+if (firstNumb > secondNumb)
+console.log(firstNumb) 
+else if (firstNumb < secondNumb)
+console.log('ошибка') 
+else if (firstNumb = secondNumb)
+console.log('Значения равны между собой') 
+
+
+let number1 = prompt ('введите число')
+number2 = prompt ('введите число')
+if (number1 > number2)
+console.log(number1)
+else if (number1 < number2)
+console.log('ошибка')
+else if (number1 = number2)
+console.log('Значения равны между собой') 
+
+
+let number = 0
+for (let number = 0; number < 100; number++) { console.log(number**2)} 
+
+
+let name1, name2, name3
+name1 = prompt('введите имя')
+name2 = prompt('введите имя')
+name3 = prompt('введите имя')
+alert (name1 + ' '+ name2 + ' ' + name3,) 
+
+
+let number1, number2, number3
+number1 = prompt('введите первое число')
+number2 = prompt('введите второе число')
+number3 = prompt('введите третье число')
+alert (+number1 + +number2 + +number3) 
+
+
+
+
